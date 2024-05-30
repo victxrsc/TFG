1a637c4f8c8c2bf65e507ada38d67a85d495e745
Víctor <victorvilascv@gmail.com>
2024-05-30 19:29:05 +0200

    Shop Buying Items

--- app/src/main/java/com/example/tfg/shop/ShopActivity.kt
+++ app/src/main/java/com/example/tfg/shop/ShopActivity.kt
@@ -1,8 +1,11 @@
 package com.example.tfg.shop
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tfg.R
 import com.google.firebase.auth.FirebaseAuth
@@ -10,63 +13,28 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 class ShopActivity : AppCompatActivity() {
 
-    private lateinit var tvShop: TextView
-    private lateinit var tvSkins: TextView
-    private lateinit var ivMonkey: ImageView
-    private lateinit var ivRaccoon: ImageView
-    private lateinit var ivKoala: ImageView
-    private lateinit var tvMonkey: TextView
-    private lateinit var tvRaccoon: TextView
-    private lateinit var tvKoala: TextView
-    private lateinit var tvPowerUps: TextView
-    private lateinit var ivX2: ImageView
-    private lateinit var ivX3: ImageView
-    private lateinit var tvX2: TextView
-    private lateinit var tvX3: TextView
-    private lateinit var tvBackgrounds: TextView
-    private lateinit var ivBackground1: ImageView
-    private lateinit var ivBackground2: ImageView
-    private lateinit var ivBackground3: ImageView
-    private lateinit var tvBackground1: TextView
-    private lateinit var tvBackground2: TextView
-    private lateinit var tvBackground3: TextView
     private lateinit var tvPoints: TextView
-
+    private var updatedPoints: Long? = null
     private var selectedImageView: ImageView? = null
-    private val originalScale = 1.0f
-    private val selectedScale = 1.2f
-
-    // Firebase
     private val database = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop)
 
-        // Asociar vistas con variables
-        tvShop = findViewById(R.id.tvShop)
-        tvSkins = findViewById(R.id.tvSkins)
-        ivMonkey = findViewById(R.id.ivMonkey)
-        ivRaccoon = findViewById(R.id.ivRaccoon)
-        ivKoala = findViewById(R.id.ivKoala)
-        tvMonkey = findViewById(R.id.tvMonkey)
-        tvRaccoon = findViewById(R.id.tvRaccoon)
-        tvKoala = findViewById(R.id.tvKoala)
-        tvPowerUps = findViewById(R.id.tvPowerUps)
-        ivX2 = findViewById(R.id.ivX2)
-        ivX3 = findViewById(R.id.ivX3)
-        tvX2 = findViewById(R.id.tvX2)
-        tvX3 = findViewById(R.id.tvX3)
-        tvBackgrounds = findViewById(R.id.tvBackgrounds)
-        ivBackground1 = findViewById(R.id.ivBackground1)
-        ivBackground2 = findViewById(R.id.ivBackground2)
-        ivBackground3 = findViewById(R.id.ivBackground3)
-        tvBackground1 = findViewById(R.id.tvBackground1)
-        tvBackground2 = findViewById(R.id.tvBackground2)
-        tvBackground3 = findViewById(R.id.tvBackground3)
         tvPoints = findViewById(R.id.tvPoints)
 
-        // Configurar listeners para los ImageView
+        loadPlayerData()
+
+        val ivMonkey = findViewById<ImageView>(R.id.ivMonkey)
+        val ivRaccoon = findViewById<ImageView>(R.id.ivRaccoon)
+        val ivKoala = findViewById<ImageView>(R.id.ivKoala)
+        val ivX2 = findViewById<ImageView>(R.id.ivX2)
+        val ivX3 = findViewById<ImageView>(R.id.ivX3)
+        val ivBackground1 = findViewById<ImageView>(R.id.ivBackground1)
+        val ivBackground2 = findViewById<ImageView>(R.id.ivBackground2)
+        val ivBackground3 = findViewById<ImageView>(R.id.ivBackground3)
+
         ivMonkey.setOnClickListener { handleSelection(ivMonkey) }
         ivRaccoon.setOnClickListener { handleSelection(ivRaccoon) }
         ivKoala.setOnClickListener { handleSelection(ivKoala) }
@@ -76,45 +44,112 @@ class ShopActivity : AppCompatActivity() {
         ivBackground2.setOnClickListener { handleSelection(ivBackground2) }
         ivBackground3.setOnClickListener { handleSelection(ivBackground3) }
 
-        // Cargar los puntos del jugador
-        loadPlayerPoints()
+        val btBuy = findViewById<Button>(R.id.btBuy)
+        btBuy.setOnClickListener { handlePurchase() }
     }
 
-    private fun handleSelection(imageView: ImageView) {
-        // Restaurar tamaño original del ImageView previamente seleccionado
-        selectedImageView?.scaleX = originalScale
-        selectedImageView?.scaleY = originalScale
-
-        // Agrandar el ImageView seleccionado
-        imageView.scaleX = selectedScale
-        imageView.scaleY = selectedScale
-
-        // Guardar el ImageView seleccionado
-        selectedImageView = imageView
-    }
-
-    private fun loadPlayerPoints() {
-        // Obtiene el email del usuario actualmente autenticado
+    private fun loadPlayerData() {
         val userEmail = FirebaseAuth.getInstance().currentUser?.email
         userEmail?.let { email ->
-            // Obtiene la referencia al documento del jugador
             val userDocumentRef = database.collection("players").document(email)
             userDocumentRef.get()
                 .addOnSuccessListener { documentSnapshot ->
-                    // Verifica si el documento existe y contiene el campo "totalPoints"
-                    if (documentSnapshot.exists() && documentSnapshot.contains("totalPoints")) {
-                        // Obtiene el valor de los puntos totales del jugador
+                    if (documentSnapshot.exists()) {
                         val totalPoints = documentSnapshot.getLong("totalPoints")
                         totalPoints?.let {
-                            // Actualiza el TextView con el valor de los puntos del jugador
-                            tvPoints.text = "Points: $totalPoints"
+                            tvPoints.text = "Points: $it"
                         }
                     }
                 }
                 .addOnFailureListener { e ->
-                    // Maneja cualquier error al cargar los puntos del jugador
-                    // Aquí puedes mostrar un mensaje de error o realizar otras acciones según sea necesario
+                    Toast.makeText(this, "Failed to load data", Toast.LENGTH_SHORT).show()
                 }
         }
+    }
+
+    private fun handleSelection(imageView: ImageView) {
+        selectedImageView?.scaleX = 1.0f
+        selectedImageView?.scaleY = 1.0f
+
+        imageView.scaleX = 1.2f
+        imageView.scaleY = 1.2f
+
+        selectedImageView = imageView
+    }
+
+    private fun handlePurchase() {
+        val selectedImageView = selectedImageView ?: run {
+            Toast.makeText(this, "No item selected", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val priceString = selectedImageView.tag as? String ?: run {
+            Toast.makeText(this, "Invalid item price", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val price = if (priceString == "Free") 0 else priceString.toLongOrNull() ?: run {
+            Toast.makeText(this, "Invalid item price format", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val userEmail = FirebaseAuth.getInstance().currentUser?.email
+        userEmail?.let { email ->
+            val userDocumentRef = database.collection("players").document(email)
+            userDocumentRef.get()
+                .addOnSuccessListener { documentSnapshot ->
+                    if (documentSnapshot.exists() && documentSnapshot.contains("totalPoints")) {
+                        val totalPoints = documentSnapshot.getLong("totalPoints") ?: return@addOnSuccessListener
+
+                        if (totalPoints >= price) {
+                            val newTotalPoints = totalPoints - price
+                            val currentItems = documentSnapshot.get("items") as? ArrayList<String> ?: arrayListOf()
+
+                            currentItems.add(getItemName(selectedImageView.id))
+
+                            userDocumentRef.update(
+                                mapOf(
+                                    "totalPoints" to newTotalPoints,
+                                    "items" to currentItems
+                                )
+                            ).addOnSuccessListener {
+                                tvPoints.text = "Points: $newTotalPoints"
+                                updatedPoints = newTotalPoints
+                                Toast.makeText(this, "Purchase successful", Toast.LENGTH_SHORT).show()
+                            }.addOnFailureListener { e ->
+                                Toast.makeText(this, "Failed to update data", Toast.LENGTH_SHORT).show()
+                            }
+                        } else {
+                            Toast.makeText(this, "Not enough points", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(this, "Failed to retrieve data", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    private fun getItemName(imageViewId: Int): String {
+        return when (imageViewId) {
+            R.id.ivMonkey -> "Monkey"
+            R.id.ivRaccoon -> "Raccoon"
+            R.id.ivKoala -> "Koala"
+            R.id.ivX2 -> "X2 PowerUp"
+            R.id.ivX3 -> "X3 PowerUp"
+            R.id.ivBackground1 -> "Background1"
+            R.id.ivBackground2 -> "Background2"
+            R.id.ivBackground3 -> "Background3"
+            else -> ""
+        }
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent()
+        updatedPoints?.let {
+            intent.putExtra("updatedPoints", it)
+        }
+        setResult(RESULT_OK, intent)
+        super.onBackPressed()
     }
 }
